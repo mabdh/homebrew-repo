696f4033084619ba2e670951816e56f11a782a93
Muhammad Abduh <mabdh.mabdh@gmail.com>
2019-07-13 17:54:58 +0700

    Revised lazy-ssh formula

--- Formula/lazy-ssh.rb
+++ Formula/lazy-ssh.rb
@@ -1,8 +1,8 @@
 class LazyConnect < Formula
   desc "Shell function to fuzzy search remote ssh address, connect to it automatically, and copy command to clipboard."
   homepage "https://github.com/mabdh/lazy-ssh"
-  url "https://github.com/mabdh/lazy-ssh/archive/v0.0.1.zip"
-  sha256 "5bc394efe7dddfe9df9d9d81cd4a738e347912c5ad7eca4a7e70ad8ae822a218"
+  url "https://github.com/mabdh/lazy-ssh/archive/v0.0.2.zip"
+  sha256 "436236414a88f7a30156608cd64ce60dffa995b6526ac144d14bb23cf3afcd07"
   depends_on "fzf" => :recommended
   def install
     bin.install "lazy-ssh"
