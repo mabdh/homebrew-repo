861d8de07d5567a867fb670392aef5c301678304
Muhammad Abduh <mabdh.mabdh@gmail.com>
2019-10-09 16:14:50 +0700

    add fzf

--- Formula/vautopn.rb
+++ Formula/vautopn.rb
@@ -4,6 +4,7 @@ class Vautopn < Formula
   url "https://github.com/mabdh/vautopn/archive/v0.0.4.zip"
   sha256 "91bed77edbf7aa686c231137d156db35d13eff20f440be9e1f2edbd73aa5b807"
   depends_on "oath-toolkit" => :recommended
+  depends_on "fzf" => :recommended
   def install
     bin.install "vautopn"
   end
