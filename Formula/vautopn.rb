06d0c95305c8d5bef3d3bdbc5bdfcdae5dcd7324
Muhammad Abduh <mabdh.mabdh@gmail.com>
2019-11-19 13:42:45 +0700

    bump up vautopn

--- Formula/vautopn.rb
+++ Formula/vautopn.rb
@@ -1,8 +1,8 @@
 class Vautopn < Formula
   desc "Shell function to fuzzy search an IPSec VPN by name and connect to it automatically."
   homepage "https://github.com/mabdh/vautopn"
-  url "https://github.com/mabdh/vautopn/archive/v0.0.5.zip"
-  sha256 "c6e8ecaf3cfae08902134f8732ddbac3e23c9afee9811d849e01f60c7ccc98de"
+  url "https://github.com/mabdh/vautopn/archive/v0.0.6.zip"
+  sha256 "5adf3922c0fd16ce5e6e99a0296635099ef5c8c2bd783aedb8c03b96cb771d48"
   depends_on "oath-toolkit" => :recommended
   depends_on "fzf" => :recommended
   def install
