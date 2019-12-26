3e23bca74fe4d00eec54a398e74b668d737aafa8
Muhammad Abduh <mabdh.mabdh@gmail.com>
2019-12-26 11:35:35 +0700

    bump up ipsvpn to v0.0.2

--- Formula/ipsvpn.rb
+++ Formula/ipsvpn.rb
@@ -1,8 +1,8 @@
 class Ipsvpn < Formula
   desc "Shell function to connect IPSec VPN via terminal in mac"
   homepage "https://github.com/mabdh/ips-vpn"
-  url "https://github.com/mabdh/ipsvpn/archive/v0.0.1.tar.gz"
-  sha256 "1dcfa07627610216fce6dedda8e44c182a9e9f9595e7b0e5af7b6d249b905f22"
+  url "https://github.com/mabdh/ipsvpn/archive/v0.0.2.tar.gz"
+  sha256 "82c68c9876adf328f108be9b1c553b3c0e740391051f19eaa9668e67e954a5f9"
   depends_on "oath-toolkit" => :recommended
   depends_on "strongswan" => :recommended
 
