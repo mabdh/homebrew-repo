607db965b701edbe48347ed0f8ceeeec2359ab64
Muhammad Abduh <mabdh.mabdh@gmail.com>
2019-12-26 21:12:00 +0700

    change v0.0.8 ssh

--- Formula/ipsvpn.rb
+++ Formula/ipsvpn.rb
@@ -2,7 +2,7 @@ class Ipsvpn < Formula
   desc "Shell function to connect IPSec VPN via terminal in mac"
   homepage "https://github.com/mabdh/ips-vpn"
   url "https://github.com/mabdh/ipsvpn/archive/v0.0.8.tar.gz"
-  sha256 "dec74e993f20d1d2797a012f28a7fde6141813278c636b0dcb2470882fc17efd"
+  sha256 "a1f8f6a047473abe50f3b41804dd684f07b2f67d7fa0a94823efbc1378c87887"
   depends_on "oath-toolkit" => :recommended
   depends_on "strongswan" => :recommended
 
