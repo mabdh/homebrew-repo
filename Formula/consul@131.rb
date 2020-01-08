9df350ddbf789d10823b2d7342e35b7ecec5808a
Muhammad Abduh <mabdh.mabdh@gmail.com>
2020-01-08 11:37:47 +0700

    add consul 1.3.1

---
+++ Formula/consul@131.rb
@@ -0,0 +1,79 @@
+class ConsulAT131 < Formula
+  desc "Tool for service discovery, monitoring and configuration"
+  homepage "https://www.consul.io"
+  url "https://github.com/hashicorp/consul.git",
+      :tag      => "v1.3.1",
+      :revision => "f2b13f3020c9c446eb9125e0cc399fafd4460d86"
+  head "https://github.com/hashicorp/consul.git",
+       :shallow => false
+
+#  bottle do
+#    cellar :any_skip_relocation
+#    sha256 "23e73f39573812be903e4130f6169e0b591395d066347a2207ce9bbae2636b81" => :mojave
+#  end
+
+  depends_on "go" => :build
+  depends_on "gox" => :build
+
+  def install
+    # Avoid running `go get`
+    inreplace "GNUmakefile", "go get -u -v $(GOTOOLS)", ""
+
+    ENV["XC_OS"] = "darwin"
+    ENV["XC_ARCH"] = "amd64"
+    ENV["GOPATH"] = buildpath
+    contents = Dir["{*,.git,.gitignore}"]
+    (buildpath/"src/github.com/hashicorp/consul").install contents
+
+    (buildpath/"bin").mkpath
+
+    cd "src/github.com/hashicorp/consul" do
+      system "make"
+      bin.install "bin/consul"
+      prefix.install_metafiles
+    end
+  end
+
+  plist_options :manual => "consul agent -dev -advertise 127.0.0.1"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/consul</string>
+          <string>agent</string>
+          <string>-dev</string>
+          <string>-advertise</string>
+          <string>127.0.0.1</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/consul.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/consul.log</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    fork do
+      exec "#{bin}/consul", "agent", "-data-dir", "."
+    end
+    sleep 3
+    system "#{bin}/consul", "leave"
+  end
+end
