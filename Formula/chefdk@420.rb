68d22cba51da890538c663edd377fc5619441f2a
Muhammad Abduh <mabdh.mabdh@gmail.com>
2020-05-18 21:42:06 +0700

    comment appcast

--- Formula/chefdk@420.rb
+++ Formula/chefdk@420.rb
@@ -3,7 +3,7 @@ class ChefDKAT420 < Formula
   sha256 "74171bc6c662bd76c405b4c6587b8f43a4783b8439bdadb711818182fa42e1fb"
 
   url "https://packages.chef.io/files/stable/chefdk/#{version}/mac_os_x/#{MacOS.version}/chefdk-#{version}-1.dmg"
-  appcast "https://www.chef.io/chef/metadata-chefdk?p=mac_os_x&pv=#{MacOS.version}&m=x86_64&v=latest&prerelease=false"
+  # appcast "https://www.chef.io/chef/metadata-chefdk?p=mac_os_x&pv=#{MacOS.version}&m=x86_64&v=latest&prerelease=false"
   name "Chef Development Kit"
   name "ChefDK"
   homepage "https://downloads.chef.io/chefdk"
--- Formula/inspec@473.rb
+++ Formula/inspec@473.rb
@@ -4,7 +4,7 @@ class InspecAT473 < Formula
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/inspec/#{version}/mac_os_x/#{MacOS.version}/inspec-#{version}-1.dmg"
-  appcast "https://github.com/chef/inspec/releases.atom"
+  # appcast "https://github.com/chef/inspec/releases.atom"
   name "InSpec by Chef"
   homepage "https://www.inspec.io/"
 
