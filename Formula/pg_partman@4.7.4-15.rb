59aca92befa9b28a1ac3c56cefe0e99d64e03521
Abduh <mabdh@users.noreply.github.com>
2024-06-12 10:41:46 +0700

    Update pg_partman@4.7.4-15.rb

--- Formula/pg_partman@4.7.4-15.rb
+++ Formula/pg_partman@4.7.4-15.rb
@@ -7,6 +7,18 @@ class PgPartmanAT47415 < Formula
 
   depends_on "postgresql@15"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7179b3094d09b2b2484d6018547100bc1130b12ec7fecd1a2def8c891642c407"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "73b11b7a1e1258bcb463fe36863e6c276739952250c492fba3628ab9cdbe4080"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7dc19da93cb2fa7f1bb168f52456a2d2410ec01a8b686f0f1d4ec2e5f90d877e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "599763c1eb5ef88cf5a42c7c0b4d12bdc87ee8f93c3e39b37d3036a98917f501"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ebc3e67b621df0dc853adecf9971ba71cd101b573ff834998588a955e8791bed"
+    sha256 cellar: :any_skip_relocation, ventura:        "9a3854be8ee43736608469f7e7913b403b40ca9b11b24be7a8eeac0661227368"
+    sha256 cellar: :any_skip_relocation, monterey:       "3f7ba9cde4ee1693b3d6db89888dfca753327a91e5140b8cf813860b896fb5ac"
+    sha256 cellar: :any_skip_relocation, big_sur:        "be5abc5ebc570d9c4fa01327ebf15c721d9b18666e800389c635d8f2cbd6e7b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f17ce22ba0ff4f9204f4a53020fc3aea3383e67432d69ed6d932e8dad4091a7"
+  end
+  
   def postgresql
     Formula["postgresql@15"]
   end
